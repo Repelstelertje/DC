60d4c5cad80bbda8fbeba156f0c8a4703705c867
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-11 13:51:02 +0200

    Set consistent CSP header

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,9 @@
   include('includes/nav_items.php');
   include('includes/config.php');
 
+  $csp = "default-src *; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://region1.google-analytics.com www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://20fhbe2020.be/ https://22mlf09mds22.com/ https://datingcontact.co.uk; style-src 'self' https://tagmanager.google.com https://fonts.googleapis.com/ https://22mlf09mds22.com/ 'unsafe-inline'; style-src-elem 'self' https://tagmanager.google.com https://fonts.googleapis.com/ https://22mlf09mds22.com/ 'unsafe-inline'; connect-src 'self' https://region1.google-analytics.com https://tagmanager.google.com/ https://www.google-analytics.com https://20fhbe2020.be/ https://22mlf09mds22.com/; script-src 'self' http://* https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com https://22mlf09mds22.com/ https://cdn.jsdelivr.net 'nonce-googletagmanager' 'nonce-2726c7f26c' 'sha256-WwSlXI54tpz3oRisOne8KKEqXFjbTYCI2AzKef7+7nE=' 'unsafe-inline' 'unsafe-eval';";
+  header("Content-Security-Policy: $csp");
+
   // Development error settings removed for production
   // ini_set('display_errors', 1);
   // ini_set('display_startup_errors', 1);
@@ -21,15 +24,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Free dating - Are you looking for a partner or a fun free date? Here you will find more than 10,000 singles. Registration is 100% free.">
     <meta name="author" content="Dating Contact">
-    <meta http-equiv="Content-Security-Policy" content="
-        default-src *; 
-        font-src 'self' https://fonts.gstatic.com;
-        img-src 'self' https://region1.google-analytics.com www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://20fhbe2020.be/ https://22mlf09mds22.com/ https://datingcontact.co.uk;
-        style-src 'self' https://tagmanager.google.com https://fonts.googleapis.com/ https://22mlf09mds22.com/ 'unsafe-inline'; 
-        style-src-elem 'self' https://tagmanager.google.com https://fonts.googleapis.com/ https://22mlf09mds22.com/ 'unsafe-inline'; 
-        connect-src 'self' https://region1.google-analytics.com https://tagmanager.google.com/ https://www.google-analytics.com https://20fhbe2020.be/ https://22mlf09mds22.com/;
-        script-src 'self' http://* https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com https://22mlf09mds22.com/ https://cdn.jsdelivr.net 'nonce-googletagmanager' 'nonce-2726c7f26c' 'sha256-WwSlXI54tpz3oRisOne8KKEqXFjbTYCI2AzKef7+7nE=' 'unsafe-inline' 'unsafe-eval'
-    ">
+    <meta http-equiv="Content-Security-Policy" content="default-src *; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://region1.google-analytics.com www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com https://20fhbe2020.be/ https://22mlf09mds22.com/ https://datingcontact.co.uk; style-src 'self' https://tagmanager.google.com https://fonts.googleapis.com/ https://22mlf09mds22.com/ 'unsafe-inline'; style-src-elem 'self' https://tagmanager.google.com https://fonts.googleapis.com/ https://22mlf09mds22.com/ 'unsafe-inline'; connect-src 'self' https://region1.google-analytics.com https://tagmanager.google.com/ https://www.google-analytics.com https://20fhbe2020.be/ https://22mlf09mds22.com/; script-src 'self' http://* https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com https://22mlf09mds22.com/ https://cdn.jsdelivr.net 'nonce-googletagmanager' 'nonce-2726c7f26c' 'sha256-WwSlXI54tpz3oRisOne8KKEqXFjbTYCI2AzKef7+7nE=' 'unsafe-inline' 'unsafe-eval';">
     <link rel="apple-touch-icon" sizes="180x180" href="img/fav/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="img/fav/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="img/fav/favicon-16x16.png">
